fd88086ec86ebe42864f18f7c827b5bbde69735c
catherine <cathey191@hotmail.com>
2018-10-30 08:59:35 +1300

    single page created

--- page.php
+++ page.php
@@ -1,7 +1,23 @@
 <?php get_header(); ?>
 
-<div class="container">
-  <h1>This is a page</h1>
-</div>
+    <div class="container">
+      <?php if(have_posts()): ?>
+        <?php while(have_posts()): the_post();?>
+          <div class="row">
+            <div class="col">
+              <h1><?= the_title(); ?></h1>
+              <hr>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div>
+                <?php the_content(); ?>
+              </div>
+            </div>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
 
 <?php get_footer(); ?>
