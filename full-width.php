5f3370284e76c3a6e096867dffb320c6351bbfde
catherine <cathey191@hotmail.com>
2018-10-30 09:20:33 +1300

    Custom page template

---
+++ full-width.php
@@ -0,0 +1,25 @@
+<?php
+  /* Template Name: Full Page Width */
+?>
+<?php get_header(); ?>
+
+    <div class="container-fluid">
+      <?php if(have_posts()): ?>
+        <?php while(have_posts()): the_post();?>
+          <div class="row">
+            <div class="col">
+              <h1><?= the_title(); ?></h1>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div>
+                <?= the_content(); ?>
+              </div>
+            </div>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </div>
+
+<?php get_footer(); ?>
