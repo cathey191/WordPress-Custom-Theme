c8edff38fd7c6e38e3c3fe6458d67d7fba97bbac
catherine <cathey191@hotmail.com>
2018-11-20 09:50:46 +1300

    displaying comments

--- single.php
+++ single.php
@@ -4,7 +4,27 @@
       <?php if(have_posts()): ?>
         <?php while(have_posts()): the_post();?>
           <?php get_template_part('content', get_post_format()); ?>
+          <div class="container">
+            <div class="col">
+              <?php
+                $comments_args = array(
+                  'comment_field' => '<div class="form-group">
+                                        <label for="comment">Enter your comment</label>
+                                        <textarea name="comment" class="form-control" aria-required="true"></textarea>
+                                      </div>'
+                );
+                comment_form($comments_args, get_the_Id());
+
+                $comments = get_comments(array(
+                  'post_id' => get_the_Id(),
+                  'status' => 'approve'
+                ));
+                wp_list_comments('', $comments);
+              ?>
+            </div>
+          </div>
         <?php endwhile; ?>
+
       <?php endif; ?>
     </div>
 
